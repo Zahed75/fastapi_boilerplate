e23f7cebc6d3735198b683c1c3cfbb6ec9eccdd4
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-06 01:02:10 +0600

    model deleted

--- models/users.py
+++ models/users.py
@@ -7,6 +7,20 @@ from pydantic import BaseModel
 from sqlalchemy.orm import relationship
 
 
+class User(BaseModel):
+    __tablename__ = 'users'
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    firstName=Column(String,nullable=False)
+    lastName=Column(String,nullable=False)
+    email=Column(String,nullable=False)
+    mobile=Column(String,nullable=False)
+    password=Column(String,nullable=False)
+    otp=Column(String,nullable=False)
+    created_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc))
+    updated_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc),onupdate=datetime.timezone.utc)
+
+
+
 
 
 
