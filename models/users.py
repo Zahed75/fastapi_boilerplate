9d75b89fa049e3fb99d47da16c7b9ec845012c6f
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-05 16:36:14 +0600

    user models updated

---
+++ models/users.py
@@ -0,0 +1,22 @@
+import datetime
+
+import nullable
+from pygments.lexer import default
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey, column, TIMESTAMP
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    firstName=Column(String,nullable=False)
+    lastName=Column(String,nullable=False)
+    email=Column(String,nullable=False)
+    mobile=Column(String,nullable=False)
+    password=Column(String,nullable=False)
+    otp=Column(String,nullable=False)
+    created_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc))
+    updated_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc),onupdate=datetime.timezone.utc)
+
+
+
