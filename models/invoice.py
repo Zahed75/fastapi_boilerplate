42f7a42198df130f7b6994f14b78f93da66076b0
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-06 21:03:16 +0600

    Invoice model added

--- models/invoice.py
+++ models/invoice.py
@@ -1,5 +1,17 @@
+import datetime
+
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, ForeignKey, TIMESTAMP
 
 
 class Invoice(BaseModel):
-    pass
+    __tablename__ = "invoice"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    customer_id = Column(Integer, ForeignKey("customer.id"))
+    total = Column(Integer, default=0)
+    discount = Column(Integer, default=0)
+    vat= Column(Integer, default=0)
+    payable = Column(Integer, default=0)
+    created_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc))
+    updated_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc))
