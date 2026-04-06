3b7c47c5b03c04c7836f3ab668c70d6595d6a36f
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-06 21:21:14 +0600

    customer model done

--- models/customers.py
+++ models/customers.py
@@ -1,5 +1,17 @@
+import datetime
+
 from pydantic import BaseModel
+from sqlalchemy import BigInteger, Column, String, TIMESTAMP
 
 
 class Customer(BaseModel):
-    pass
+    __tablename__ = "customers"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    mobile = Column(String, nullable=False)
+    user_id = Column(BigInteger, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc))
+    updated_at=Column(TIMESTAMP,nullable=False,default=lambda:datetime.now(datetime.timezone.utc))
+
