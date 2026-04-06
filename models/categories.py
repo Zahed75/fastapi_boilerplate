cc0ee8886811dfa5781a66d0587245a8df09cfed
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-06 19:58:16 +0600

    categories model done

--- models/categories.py
+++ models/categories.py
@@ -1,5 +1,14 @@
+import datetime
 from pydantic import BaseModel
+from sqlalchemy import BigInteger, Column, TIMESTAMP, String, ForeignKey
 
 
 class Category(BaseModel):
-    pass
+    __tablename__ = 'categories'
+    id = Column(BigInteger, primary_key=True,autoincrement=True)
+    name = Column(String,nullable=False)
+    user_id=Column(BigInteger,ForeignKey('users.id'),nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc))
+    updated_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc),
+                        onupdate=datetime.timezone.utc)
+
--- models/users.py
+++ models/users.py
@@ -6,22 +6,18 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     __tablename__ = 'users'
-    id=Column(Integer,primary_key=True,autoincrement=True)
-    firstName=Column(String,nullable=False)
-    lastName=Column(String,nullable=False)
-    email=Column(String,nullable=False)
-    mobile=Column(String,nullable=False)
-    password=Column(String,nullable=False)
-    otp=Column(String,nullable=False)
-    created_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc))
-    updated_at=Column(TIMESTAMP, nullable=False, default=lambda:datetime.now(datetime.timezone.utc),onupdate=datetime.timezone.utc)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    firstName = Column(String, nullable=False)
+    lastName = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    mobile = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    otp = Column(String, nullable=False)
 
     category = relationship("Category", back_populates='user')
-    products=relationship("Product", back_populates='user')
-    invoices=relationship("InvoiceProduct", back_populates='user')
-
-
-
-
-
-
+    products = relationship("Product", back_populates='user')
+    invoices = relationship("InvoiceProduct", back_populates='user')
+    customer = relationship("Customer", back_populates='user')
+    created_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc))
+    updated_at = Column(TIMESTAMP, nullable=False, default=lambda: datetime.now(datetime.timezone.utc),
+                        onupdate=datetime.timezone.utc)
