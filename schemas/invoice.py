f5be56c06f6934728ab10264e64a319de4a53cb6
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-08 03:37:10 +0600

    pydantic model updated invoice

--- schemas/invoice.py
+++ schemas/invoice.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
 
-class Invoice(BaseModel):
+class InvoiceCreate(BaseModel):
+    customer_id: int
+    total:float
+    discount:float
+    vat:float
+    payable:float
+
+
+
+class InvoiceUpdate(BaseModel):
     customer_id: int
     total:float
     discount:float
