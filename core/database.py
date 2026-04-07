51e40b6f2a4a79445700c8ddb2841934863988dd
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-07 22:05:00 +0600

    database autoflash added

--- core/database.py
+++ core/database.py
@@ -5,4 +5,4 @@ from sqlalchemy.orm import scoped_session,sessionmaker
 DATABASE_URL = "postgresql://username:password@localhost:5432/database_name"
 
 engine = create_engine(DATABASE_URL,pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False,autoflush=True,bind=engine)
