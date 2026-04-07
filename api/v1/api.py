2b18f77732dc5ea2f9f77d5cca84d795d87acffd
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-07 20:43:10 +0600

    swagger implement

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,14 +1,14 @@
-from endpoints import auths,categories,dashboard,invoices,invoice_items,users
-
+from endpoints import auths,categories,dashboard,invoices,invoice_items,users,products
 
 from fastapi import APIRouter
 router = APIRouter()
 
-router.include_router(auths.router)
-router.include_router(categories.router)
-router.include_router(dashboard.router)
-router.include_router(invoices.router)
-router.include_router(invoice_items.router)
-router.include_router(users.router)
-router.include_router(invoices.router)
-router.include_router(invoice_items.router)
+router.include_router(auths.router,tags=["auths"])
+router.include_router(categories.router,tags=["categories"])
+router.include_router(dashboard.router,tags=["dashboard"])
+router.include_router(invoices.router,tags=["invoices"])
+router.include_router(invoice_items.router,tags=["invoice_items"])
+router.include_router(users.router,tags=["users"])
+router.include_router(invoices.router,tags=["invoices"])
+router.include_router(invoice_items.router,tags=["invoice_items"])
+router.include_router(products.router,tags=["products"])
