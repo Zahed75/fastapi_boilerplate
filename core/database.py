8014a659fd827e09f2c72e227271b16504999030
“Zahed”75 <zahedhasan.cs@gmail.com>
2026-04-07 22:04:16 +0600

    database config added

---
+++ core/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+
+
+DATABASE_URL = "postgresql://username:password@localhost:5432/database_name"
+
+engine = create_engine(DATABASE_URL,pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
